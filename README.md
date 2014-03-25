789fd8f7a096437c8eb18143da9cd0cd522ceec2
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-03-25 12:21:59 +0800

    initial version

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ git-webhook
 ===========
 
 Github webhook written in Node.js
+
+## Usage
+
+- rename `config.js.example` to `config.js`
+- edit `config.js` for your need
+- `node hook.js`
---
+++ hook.js
@@ -0,0 +1,20 @@
+
+var config = require('./config.js')
+var gith = require('gith').create(config.bind_port);
+var exec = require('child_process').exec;
+
+gith({
+    repo: 'fideloper/example'
+}).on('all', function(payload) {
+    // console.log(payload)
+    exec('cd ' + config.repo_location + ' && git pull ' + config.repo_remote, function(err, stdout, stderr) {
+	if (err) throw err;
+	if (config.additional_cmd) {
+	    exec(additional_cmd, function(err, stdout, stderr) {
+		if (err) throw err;
+		console.log('Command complete.');
+	    });
+	}
+	console.log('done.')
+    });
+});
